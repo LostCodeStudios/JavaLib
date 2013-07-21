bb54a65aebfc9be48c7c94794229e149e876f5b0
William Guss <carlostnb@gmail.com>
2013-07-21 16:20:46 -0600

    Removed TesterConsole. Set up JUNIT Testing (EntityTest)

---
+++ JavaLib/src/com/punchline/javalib/tests/EntityTest.java
@@ -0,0 +1,16 @@
+package com.punchline.javalib.tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class EntityTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
