b0673c30e11ba7c6d6b0008d9af2a34f35f956af
William Guss <carlostnb@gmail.com>
2013-07-23 11:06:56 -0600

    Changing Component.java to interface. fuck nat..

--- JavaLib/src/com/punchline/javalib/entities/Component.java
+++ JavaLib/src/com/punchline/javalib/entities/Component.java
@@ -1,22 +1,22 @@
 package com.punchline.javalib.entities;
 
 /**
- * Base class for components.
+ * Base interface for components.
  * @author Nathaniel
  *
  */
-public abstract class Component {
+public interface Component {
 	
 	/**
 	 * Called when this Component is added to a {@link ComponentManager}.
 	 * @param container The {@link ComponentManager} this Component is added to.
 	 */
-	public void onAdd(ComponentManager container) { }
+	void onAdd(ComponentManager container);
 	
 	/**
 	 * Called when this Component is removed from a {@link ComponentManager}.
 	 * @param container The {@link ComponentManager} this Component is removed from.
 	 */
-	public void onRemove(ComponentManager container) { }
+	void onRemove(ComponentManager container);
 	
 }
