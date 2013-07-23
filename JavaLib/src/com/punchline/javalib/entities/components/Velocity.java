08c021d753c01fe3b9a8d3559c793c427f9ba8bc
William Guss <carlostnb@gmail.com>
2013-07-23 11:24:08 -0600

    Fixing last little issues with RenderSystem.java, Body.java, Transform.java, and Velocity.java

--- JavaLib/src/com/punchline/javalib/entities/components/Velocity.java
+++ JavaLib/src/com/punchline/javalib/entities/components/Velocity.java
@@ -3,10 +3,12 @@
  */
 package com.punchline.javalib.entities.components;
 
+import com.punchline.javalib.entities.Component;
+
 /**
  * @author William
  * @created Jul 23, 2013
  */
-public interface Velocity {
+public interface Velocity extends Component{
 	
 }
