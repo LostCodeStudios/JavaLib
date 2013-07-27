d789a7f77e7d714332d1b196a041ec63eb02ed82
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-07-27 12:08:34 -0600

    EntityWorld game over.

---
+++ JavaLib/src/com/punchline/javalib/entities/GameOverInfo.java
@@ -0,0 +1,11 @@
+package com.punchline.javalib.entities;
+
+/**
+ * Contains all relevant information about why the game ended. Can be extended to provide functionality such as
+ * opening a specific victory screen announcing who won the game.
+ * @author Nathaniel
+ * @created Jul 27, 2013
+ */
+public class GameOverInfo {
+	
+}
