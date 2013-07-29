61eeba5d3d91a7b897822403331d92c76feb496b
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-07-29 13:31:56 -0600

    TrackingCameraSystem and DebugRenderSystem scale properly.

--- JavaLib/src/com/punchline/javalib/entities/systems/generic/TrackingCameraSystem.java
+++ JavaLib/src/com/punchline/javalib/entities/systems/generic/TrackingCameraSystem.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.punchline.javalib.entities.Entity;
 import com.punchline.javalib.entities.components.physical.Transform;
 import com.punchline.javalib.entities.systems.TagSystem;
+import com.punchline.javalib.utils.Convert;
 
 /**
  * System for moving the camera to follow a given entity.
@@ -32,29 +33,29 @@ public class TrackingCameraSystem extends TagSystem {
 	protected void process(Entity e) {
 		Transform t = (Transform)e.getComponent(Transform.class);
 		
-		Vector2 pos = t.getPosition();
+		Vector2 pos = Convert.metersToPixels(t.getPosition());
 		
 		camera.position.set(pos.x, pos.y, 0f);
 		
-		float left = camera.position.x - camera.viewportWidth / 2;
-		float bottom = camera.position.y - camera.viewportHeight / 2;
-		float right = left + camera.viewportWidth;
-		float top = bottom + camera.viewportHeight;
+		float left = camera.position.x - Convert.pixelsToMeters(camera.viewportWidth / 2);
+		float bottom = camera.position.y - Convert.pixelsToMeters(camera.viewportHeight / 2);
+		float right = left + Convert.pixelsToMeters(camera.viewportWidth);
+		float top = bottom + Convert.pixelsToMeters(camera.viewportHeight);
 		
 		if (left < bounds.x) {
-			camera.position.x = bounds.x + camera.viewportWidth / 2;
+			camera.position.x = bounds.x + Convert.pixelsToMeters(camera.viewportWidth / 2);
 		}
 		
-		if (top > bounds.y + camera.viewportHeight) {
-			camera.position.y = bounds.y + camera.viewportHeight - camera.viewportHeight / 2;
+		if (top > bounds.y + bounds.height) {
+			camera.position.y = bounds.y + bounds.height -  Convert.pixelsToMeters(camera.viewportHeight / 2);
 		}
 		
 		if (right > bounds.x + bounds.width) {
-			camera.position.x = bounds.x + bounds.width - camera.viewportWidth / 2;
+			camera.position.x = bounds.x + bounds.width - Convert.pixelsToMeters(camera.viewportWidth / 2);
 		}
 		
 		if (bottom < bounds.y) {
-			camera.position.y = bounds.y + camera.viewportHeight / 2;
+			camera.position.y = bounds.y + Convert.pixelsToMeters(camera.viewportHeight / 2);
 		}
 	}
 
