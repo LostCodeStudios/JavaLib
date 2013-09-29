8f39e9fc751f5d668b8c968ba24d2361ec56a7b9
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-09-29 13:41:31 -0600

    Starting a new song stops the old one instead of pausing it.

--- JavaLib/src/com/punchline/javalib/utils/SoundManager.java
+++ JavaLib/src/com/punchline/javalib/utils/SoundManager.java
@@ -181,7 +181,7 @@ public final class SoundManager {
 	public static void playSong(String key, float volume, boolean looping) {
 		if (volumeInvalid(volume)) return;
 		
-		if (isMusicPlaying()) currentSong.pause();
+		if (isMusicPlaying()) currentSong.stop();
 		
 		Music song = songs.get(key);
 		song.setLooping(looping);
