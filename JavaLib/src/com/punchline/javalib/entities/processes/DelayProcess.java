e1f7e94c86d8044d0270b7d5b738e273da0805b0
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-09-30 14:30:01 -0600

    Created DelayProcess.

---
+++ JavaLib/src/com/punchline/javalib/entities/processes/DelayProcess.java
@@ -0,0 +1,43 @@
+package com.punchline.javalib.entities.processes;
+
+import com.punchline.javalib.entities.EntityWorld;
+
+/**
+ * A {@link Process} that simply waits for a specified amount of time before triggering its child processes.
+ * @author Natman64
+ *
+ */
+public class DelayProcess extends Process {
+	
+	//region Fields
+	
+	private float time;
+	
+	//endregion
+	
+	//region Initialization
+	
+	public DelayProcess(float time, Process... children) {
+		this.time = time;
+		
+		for (Process child : children) {
+			attachChild(child);
+		}
+	}
+	
+	//endregion
+	
+	//region Update
+	
+	@Override
+	public void update(EntityWorld world, float deltaTime) {
+		time -= deltaTime;
+		
+		if (time <= 0) {
+			end(ProcessState.SUCCEEDED);
+		}
+	}
+
+	//endregion
+	
+}
