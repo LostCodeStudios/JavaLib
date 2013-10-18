18e2b7db06e6f60f7d0700e35fa65483ec415c8a
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-10-17 20:46:18 -0600

    ExpirationProcess expanded to end if the Entity deletes itself first.

--- JavaLib/src/com/punchline/javalib/entities/processes/ExpirationProcess.java
+++ JavaLib/src/com/punchline/javalib/entities/processes/ExpirationProcess.java
@@ -1,6 +1,8 @@
 package com.punchline.javalib.entities.processes;
 
 import com.punchline.javalib.entities.Entity;
+import com.punchline.javalib.entities.EntityWorld;
+import com.punchline.javalib.entities.events.processes.EndProcessCallback;
 
 /**
  * Deletes an Entity after a specified amount of time ends.
@@ -9,6 +11,8 @@ import com.punchline.javalib.entities.Entity;
  */
 public class ExpirationProcess extends DelayProcess {
 
+	private Entity e;
+	
 	/**
 	 * Creates an ExpirationProcess.
 	 * @param time The amount of seconds to wait before deleting
@@ -16,6 +20,16 @@ public class ExpirationProcess extends DelayProcess {
 	 */
 	public ExpirationProcess(float time, Entity e) {
 		super(time, new DeletionProcess(e));
+		
+		e.onDeleted.addCallback(this, new EndProcessCallback(this, ProcessState.FAILED));
+		this.e = e;
+	}
+
+	@Override
+	public void onEnd(EntityWorld world, ProcessState endState) {
+		if (endState == ProcessState.ABORTED) {
+			e.onDeleted.removeCallback(this);
+		}
 	}
 
 }
